a5cd2ddddb41a89043e20565b4876e2cb3277a67
Sangjee Dondrub <sangjeedondrub@live.com>
2022-04-11 00:08:30 +0800

    (c) Microsoft 1988

--- example-app.cpp
+++ example-app.cpp
@@ -1,9 +1,13 @@
 #include <torch/torch.h>
 #include <iostream>
 
-using namespace std;
-
 int main() {
   torch::Tensor tensor = torch::rand({2, 3});
+  torch::Tensor result = tensor.multiply(tensor);
+
+  std::cout << "tensor"  << "\n";
   std::cout << tensor << std::endl;
+  std::cout << "tensor multiplied"  << "\n";
+  std::cout << result << std::endl;
+
 }
