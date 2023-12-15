e7da935702981550112f9625884a2242b1b26e4e
Sangjee Dondrub <sangjeedondrub@live.com>
2023-12-16 02:59:05 +0800

    trying to add opencv4 and ffmpeg 6, not finished.

--- example-app.cpp
+++ example-app.cpp
@@ -1,13 +1,19 @@
-#include <torch/torch.h>
 #include <iostream>
+#include <torch/torch.h>
+
+
+
 
 int main() {
   torch::Tensor tensor = torch::rand({2, 3});
   torch::Tensor result = tensor.multiply(tensor);
 
-  std::cout << "tensor"  << "\n";
+  std::cout << "tensor"
+	    << "\n";
   std::cout << tensor << std::endl;
-  std::cout << "tensor multiplied"  << "\n";
+  std::cout << "tensor multiplied"
+	    << "\n";
   std::cout << result << std::endl;
 
+  return 0;
 }
